4319824d8f505ff32e346ceeeac90a885ac44906
southernwind <testakina@gmail.com>
2019-11-10 13:33:17 +0900

    サーバー接続まで

--- RavenDbTest/Program.cs
+++ RavenDbTest/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+using System.Threading.Tasks;
 
-namespace RavenDbTest
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
+using Raven.Client.ServerWide;
+using Raven.Embedded;
+
+namespace RavenDbTest {
+	internal class Program {
+		private static async Task Main(string[] args) {
+			var so = new ServerOptions {
+				DataDirectory = "RavenData"
+			};
+			EmbeddedServer.Instance.StartServer(so);
+			var dbo = new DatabaseOptions(
+				new DatabaseRecord { DatabaseName = "Embedded" }
+			);
+			using var store = await EmbeddedServer.Instance.GetDocumentStoreAsync(dbo);
+			using var session = store.OpenSession();
+
+			var url = await EmbeddedServer.Instance.GetServerUriAsync();
+
+			EmbeddedServer.Instance.OpenStudioInBrowser();
 		}
 	}
 }
